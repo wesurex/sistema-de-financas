312e3578d0cc85da33fb16905e826efbf7fe9ca0
wesley reinert da silva <wesley.aqw@gmail.com>
2024-08-06 19:08:57 -0300

    first commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,17 @@
+const nameEvent = document.querySelector('#nameEvent'),
+    dateEvent = document.querySelector('#dateEvent'),
+    valueEvent = document.querySelector('#valueEvent'),
+    description = document.querySelector('#description'),
+    btnSubmit = document.querySelector('#saveData'),
+    tableReturn = document.querySelector('#tableReturn');
+
+let save = []
+
+    function saveData () {
+        const nameEventValue = nameEvent.value,
+            dateEventValue = dateEvent.value,
+            valueEventValue = valueEvent.value,
+            descriptionValue = description.value;
+
+            
+    } 
